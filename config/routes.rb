e1b5ba26c7b5a25cfc5a669f327967143f24ece3
Stephen Suen <ssuen@mit.edu>
2013-09-28 22:30:28 -0400

    Generated Note scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SsuenProj2::Application.routes.draw do
+  resources :notes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
