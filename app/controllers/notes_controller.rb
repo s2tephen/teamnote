3c87961c749dcaf5f4a2cf0a23e83be3cdabf68b
Stephen Suen <ssuen@mit.edu>
2013-09-30 08:24:07 -0400

    Added slug field and implemented table sorting.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -25,6 +25,7 @@ class NotesController < ApplicationController
   # POST /notes.json
   def create
     @note = Note.new(note_params)
+    @note.slug = @note.title.parameterize.truncate(100, :separator => '-', :omission => '')
 
     respond_to do |format|
       if @note.save
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130929022255) do
+ActiveRecord::Schema.define(version: 20130929073703) do
 
   create_table "notes", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
 end
