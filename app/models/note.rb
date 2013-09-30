cfe0d5c60525c39b3446e8600e14bf059fe8f5f1
Stephen Suen <ssuen@mit.edu>
2013-09-30 14:51:30 -0400

    Form and button styling, content generator function.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,17 @@
 class Note < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  # Bulk-generates notes with lorem ipsum.
+  # Assumes that n is an integer.
+  # n: number of notes to generate
+  def self.generate(n)
+    require 'lorem-ipsum'
+    n.times do
+      body = LoremIpsum.generate
+      head = body.split(/\.|,/).last.truncate(40, :separator => ' ', :omission => '')[1..-1].capitalize
+      note = Note.new(:title => head, :content => body)
+      note.save
+    end
+  end
 end
